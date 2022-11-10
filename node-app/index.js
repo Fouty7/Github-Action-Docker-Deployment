34c40734988077c10c8b58a58c2a6ce357a5c2ef
Aminu <Amin4712@gmail.com>
2022-11-10 14:40:57 +0100

    adding mock test

--- node-app/index.js
+++ node-app/index.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    express=res.send('Hello World!')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
+
+function test () {
+    return ("Hello World!")
+}
+module.exports = test;
