de7a29cdf8757cc8ab62ed30dda3720d139d31a2
Aminu <Amin4712@gmail.com>
2022-11-11 01:25:15 +0100

    running playbook

--- node-app/index.js
+++ node-app/index.js
@@ -3,14 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    express=res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
-
-function test () {
-    return ("Hello World!")
-}
-module.exports = test;
