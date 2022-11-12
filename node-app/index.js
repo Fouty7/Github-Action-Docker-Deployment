ec616bb7aa87f75b1789e9db9241ccf44c19aaf2
Aminu <Amin4712@gmail.com>
2022-11-12 04:44:27 +0100

    workflow

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! FROM GITHUB ACTION-testing code change')
+    res.send('Hello World! FROM GITHUB ACTION testing code change')
 })
 
 app.listen(port, () => {
