0adf6dfba23ce0863ec1d7f1905dfd3ae6f8799c
Aminu <Amin4712@gmail.com>
2022-11-21 19:32:56 +0100

    optimizing wokflow pipeline

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! FROM GITHUB ACTION!! Confiming Workflow optimization')
+    res.send('Hello World! FROM GITHUB ACTION!! Confiming Workflow optimization// testing git clone recurssion')
 })
 
 app.listen(port, () => {
