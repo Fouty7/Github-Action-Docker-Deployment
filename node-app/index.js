c125ccf7d6842d6be1b98f56ca269c708791a418
Aminu Ndakun <amin4712@gmail.com>
2023-04-02 12:05:51 +0100

    testing quality gate

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! FROM GITHUB ACTION!! Testing sonar Quality gate')
+    res.send('Hello World! FROM GITHUB ACTION!! Testing sonar Quality gate - 001')
 })
 
 app.listen(port, () => {
