c928e710d4033ea4c04ed7743e3c4891002ab533
Aminu Ndakun <amin4712@gmail.com>
2023-04-02 12:19:53 +0100

    bugfix

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! FROM GITHUB ACTION!! Testing sonar Quality gate - 001')
+    res.send('Hello World! FROM GITHUB ACTION!! Testing sonar Quality gate - 002')
 })
 
 app.listen(port, () => {
